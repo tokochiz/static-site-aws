ab9be5de092922104b082a9991027928c4093579
tokochiz <tokohana.1003@gmail.com>
2025-02-02 00:03:23 +0900

    ADD: node_modules add

---
+++ portfolio-website/src/utils/constants.js
@@ -0,0 +1,11 @@
+export const S3_IMAGES = [
+    'https://tokochiz-static-web-dev.s3.us-east-1.amazonaws.com/photos/original/dog_photo1.webp',
+    'https://tokochiz-static-web-dev.s3.us-east-1.amazonaws.com/photos/original/dog_photo1.webp',
+    'https://tokochiz-static-web-dev.s3.us-east-1.amazonaws.com/photos/original/dog_photo1.webp',
+ ];
+  
+  export const PROJECTS = [
+    { id: 1, title: 'Project 1' },
+    { id: 2, title: 'Project 2' },
+    { id: 3, title: 'Project 3' },
+  ];
